8bb4544d81edd63d3a6a08d7e979905bfc99b8c7
Darius <dariustinculescu@gmail.com>
2022-11-18 03:12:47 +0200

    Created RoleService.ts

--- client/student-management/src/services/RoleService.ts
+++ client/student-management/src/services/RoleService.ts
@@ -1,4 +1,10 @@
-export interface IRole {
-    id: number;
-    roleName: string;
+import axios from 'axios';
+import IRole from '../models/Role';
+
+const BASE_URL = `${process.env.REACT_APP_BASE_API_URL}/roles`;
+
+const findAll = async function(): Promise<IRole[]>{
+     const response = await axios.get(BASE_URL);
+
+     return response.data.data;
 }
